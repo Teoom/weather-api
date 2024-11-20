d5acf16e5551ee685beeb96b6d28a91774252c5f
Teoom <ararama199915@gmail.com>
2024-11-20 16:19:12 +0700

    add api geocoder

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,26 @@
+import  API__KEYS from "./variables.js";
+
+const { GEOCODDER__API } = API__KEYS
+
 const getLocationBtn = document.querySelector(".location-btn")
 
-getLocationBtn.addEventListener('click', () => {
+getLocationBtn.addEventListener('click',  () => {
   const geolocation = navigator.geolocation;
-  geolocation.getCurrentPosition(position => {
+  geolocation.getCurrentPosition(async (position) => {
     console.log(position)
     console.log(new Date().getTime())
     const { latitude, longitude } = position.coords;
     console.log(latitude, longitude)
+    
+    let data  = await fetch(`https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&format=json&api_key=${GEOCODDER__API}`)
+    console.log(await data.json())
+
+    let cor = await fetch(`https://geocode.maps.co/search?q=Krasnoyarsk&api_key=673da07d240d1824659150roze6743e`)
+    console.log(await cor.json())
   })
 })
+
+
 
 
 
--- src/scripts/variables.js
+++ src/scripts/variables.js
@@ -1,1 +1,7 @@
-const GEOCODDER__API = "";
+
+
+const API__KEYS = {
+    GEOCODDER__API: '673da07d240d1824659150roze6743e',
+}
+
+export default API__KEYS;
